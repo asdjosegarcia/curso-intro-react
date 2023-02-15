41f5efd439a61f46aa81dac73a74e3e1306777cf
asdjosegarcia <asdjosegarcia@gmail.com>
2023-02-14 22:47:10 -0300

    clase 15: Manejo de efectos(useEffect) -se agregro el hook useEffect(actualiza modulos cuando detecta cambios en variables) -cambios en App.js, AppUI.js

--- src/App/App.js
+++ src/App/App.js
@@ -8,34 +8,60 @@ import { AppUI } from './AppUI';
   { text: 'algo no se que ', completed: true }
 ]; */
 
-function useLocalStorage(itemName,initialValue){//esta funcion nos permite cargar objetos a local storage
-  
-  const localStorageItem=localStorage.getItem(itemName );//se extraen los valores del localStorage>TODOS_v1 del navegador y se guardan en la variable LocalStorageTodoss
-  let parsedItem;//aqui se van a guardar los objetos en formato JS
-  
-  if(!localStorageItem){//si localStorageItem NO tiene algun valor
-    localStorage.setItem(itemName ,JSON.stringify(initialValue));//creamos el objeto itemName con un array vacio en formato JSON en el localStorage
-    parsedItem=initialValue;
-  }else{
-    parsedItem=JSON.parse(localStorageItem)//transforma los objetos de JSON a JS y los guarda en parsedItem
-  }
-  
-  const [item, setItem] = React.useState(parsedItem)//estado con el valor de la variable defaultTodo por defecto
-  
-  const saveItem=(newItem)=>{
-    const stringifiedItem=JSON.stringify(newItem);//netItem transforma a JSON y almacena en strinfigiedItem
-    localStorage.setItem(itemName ,stringifiedItem);//se carga stringifiedItem en localStorage
+function useLocalStorage(itemName, initialValue) {//esta funcion nos permite cargar objetos a local storage
+  const [error, setError] = React.useState(false);//sumular estado de error, estado inicial false para decir no hay error
+  const [loading, setLoading] = React.useState(true);//simular estado de carga, true para simular que ya termino de cargar
+  const [item, setItem] = React.useState(initialValue)//estado con el valor de la variable defaultTodo por defecto
+
+  React.useEffect(() => { //useEffect va a recargar todo cuando detecte un cambio, asi vemos los cambios en loading, error, etc
+    try {
+      setTimeout(() => {//usaremos setTimeout para simular el funcionamiento de una api, (reciviendo la informacion con restraso)
+        const localStorageItem = localStorage.getItem(itemName);//se extraen los valores del localStorage>TODOS_v1 del navegador y se guardan en la variable LocalStorageTodoss
+        let parsedItem;//aqui se van a guardar los objetos en formato JS
+
+        if (!localStorageItem) {//si localStorageItem NO tiene algun valor
+          localStorage.setItem(itemName, JSON.stringify(initialValue));//creamos el objeto itemName con un array vacio en formato JSON en el localStorage
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem)//transforma los objetos de localStorage tipo JSON a JS y los guarda en parsedItem
+        }
+
+        setItem(parsedItem)//establecemos el item como parsedItem (lo que tenia adentro localStorage)
+        setLoading(false)//establecemos loading como false(la app ya termino de cargar)
+      }, 1000)
+      } catch (error) {//si hay un error entra como parametro
+        setError(error)
+    }
+  });
+
+
+  const saveItem = (newItem) => {
+    try{
+      const stringifiedItem = JSON.stringify(newItem);//netItem transforma a JSON y almacena en strinfigiedItem
+    localStorage.setItem(itemName, stringifiedItem);//se carga stringifiedItem en localStorage
     setItem(newItem);// se cargan los nuevos todos en todos
+    }catch(error){
+      setError(error)
+    }
   }
-  return [
+
+
+  return {//si tenemos mas de 2 estados en el custom react hook es recomendable mandarlo como objeto y no como array
     item,
     saveItem,
-  ]
+    loading,
+    error,
+  }
 }
 
 
 function App() {
-  const [todos,saveTodos]=useLocalStorage('TODOS_V1',[])//'TODOS_V1',[] son los argumentos,todo es item, saveItem es save todos, estamos declarando como constante lo que retorna de la funcion
+  const {
+    item: todos,//renombre a item como todos, esto es necesario por como llamamos(llamar de nombre) a las funciones en function App
+    saveItem: saveTodos,
+    loading,
+    error
+  } = useLocalStorage('TODOS_V1', [])//'TODOS_V1',[] son los argumentos,todo es item, saveItem es save todos, estamos declarando como constante lo que retorna de la funcion
 
   const [searchValue, setSearchValue] = React.useState('')//se lo enviamos a serach.js para que le agrege un valor, 
 
@@ -68,7 +94,9 @@ function App() {
 
   return (
     <AppUI
-      totalTodos={totalTodos}
+      loading={loading}//mandamos a appUI la variable loading con el estado de carga
+      error={error}
+      totalTodos={totalTodos}/* todas estas variables van a AppUI.js */
       completedTodos={completedTodos}
       searchValue={searchValue}
       setSearchValue={setSearchValue}
@@ -78,5 +106,6 @@ function App() {
     />
   );
 }
+
 
 export default App;
--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { TodoCounter } from "../TodoCounter/TodoCounter";
-import { TodoSearch } from "../TodoSearch/TodoSearch.js";
+import { TodoSearch } from "../TodoSearch/TodoSearch.js";/* importamos los moddulos para extrar esus variables */
 import { TodoList } from "../TodoList/TodoList.js";
 import { TodoItem } from "../TodoItem/TodoItem.js";
 import { CreateTodoButton } from "../CreateTodoButton/CreateTodoButton.js";
 function AppUI({
-    totalTodos,
+    loading,/* llamamos a loading y error a pesar d eque no existen para que el programa tome como que si las declaramos en algun mmomento */
+    error,
+    totalTodos,/* variables que nos envia App.js */
     completedTodos,
     searchValue,
     setSearchValue,
@@ -24,6 +26,9 @@ function AppUI({
                 setSearchValue={setSearchValue}//envamos la funcion para que pueda cambiar el valor de el estado
             />
             <TodoList>
+                {error && <p>Desesperate hubo un error</p> }{/* imaginamos que tenemos una variable loading con un error */}
+                {loading && <p>Estamos cargando, no desesperes...</p> }{/* imaginamos que tenemos una variable loading */}
+                {(!loading && !searchedTodos.length)&& <p>Crea tu primer todo!</p>}{/* imaginamos qie todo cargo, loading no tiene nada y searchedTodos tampoco */}
                 {searchedTodos.map(
                     todo => (<TodoItem
                         key={todo.text} /* key agrega un identificador unico que sirve para listas, evita que se re-renderize toda la lista,  solo renderiza los elementos que necesite, */
